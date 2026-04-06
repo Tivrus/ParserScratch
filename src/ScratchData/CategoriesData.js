4ed6267ba67b0d95a54ed06da9485abf6d55fa14
Tivrus <tivruspod4@gmail.com>
2026-04-06 23:08:15 +0300

    v12.1.1

--- src/ScratchData/CategoriesData.js
+++
@@ -1,7 +0,0 @@
-export const categories_list = [
-    { id: "Motion", color: "#4C97FF", text: "Движения" },
-    { id: "Looks", color: "#9966FF", text: "Внешний вид" },
-    { id: "Sound", color: "#CF63CF", text: "Звуки" },
-    { id: "Events", color: "#FFBF00", text: "События" },
-    { id: "Control", color: "#FFAB19", text: "Управление" }
-];
